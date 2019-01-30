8583230579bbe3797f940361b66eaa62c11ab313
Vue <1831860861@qq.com>
2019-01-30 21:53:37 +0800

    add city-list,but something is wrong with the city.json

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,47 @@
   <div> <!-- 自定义的组件需要外面包一个div标签 -->
     <city-header></city-header>
     <city-search></city-search>
+    <city-list :hot="hotCities" :cities="cities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
 export default {
   name: 'City',
+  data () {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphabet
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json') // 无法正常导入，原因应该是代码过长括号匹配出错
+        .then(this.getCityInfoSuc)
+    },
+    getCityInfoSuc (res) {
+      res = res.data
+      if (res.ref && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="list" ref="wrapper"> <!-- ref获取DOM -->
+    <div>
+      <div class="area">
+        <div class="title boder-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title boder-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper" v-for="item of hot" :key="item.id">
+            <div class="button">{{item.name}}</div>
+          </div>
+        </div>
+      </div>
+      <div class="area" v-for="(item, key) of cities" :key="key">
+        <div class="title boder-topbottom">{{key}}</div>
+          <div class="item-list">
+            <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">
+              {{innerItem.name}}
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll' // 导入better-scroll插件，详情见github
+export default {
+  name: 'CityList',
+  props: {
+    hot: Array,
+    cities: Object
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .boder-topbottom // 使1像素边框颜色加深
+    &:before
+      border-color #ccc
+    &:after
+      border-color #ccc
+  .border-bottom
+    &:before
+      border-color #ccc
+  .list
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+  .title
+    background-color: #eee
+    line-height .54rem
+    padding-left .2rem
+    color #666
+    font-size .26rem
+  .button-list
+    overflow hidden // 触发BFC
+    padding .1rem .6rem .1rem .1rem // 给右侧字母排序留空间
+    .button-wrapper
+      float left
+      width 33.3%
+      .button
+        border-radius .06rem
+        border .02rem solid #ccc
+        padding .1rem 0
+        text-align center
+        margin .1rem
+  .item-list
+    .item
+      line-height .76rem
+      padding-left .2rem
+</style>

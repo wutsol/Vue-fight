61126de7f5df71e3318e4bf2b76d460574dcba30
Vue <1831860861@qq.com>
2019-02-01 20:04:24 +0800

    add detail-list with 递归组件

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,39 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [ {
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人三馆联票之终极联票'
+          }]
+        }]
+      }, {
+        title: '成人票'
+      }, {
+        title: '成人票'
+      }]
+    }
   }
 }
 </script>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="item" v-for="(item, index) of list" :key="index">
+      <div class="item-title boder-bottom">
+        <span class="item-title-icon"></span>
+        {{item.title}}
+      </div>
+      <div v-if="item.children">
+        <detail-list :list="item.children" class="item-chlidren"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script> // 该组件实现递归组件的使用，即item有次级list时复用组件进行加载
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .item-title
+    line-height .8rem
+    font-size .32rem
+    padding 0 .2rem
+  .item-title-icon
+    display inline-block
+    position relative
+    left .06rem
+    top .06rem
+    width .36rem
+    height .36rem
+    background url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+    margin-right .1rem
+    background-size .4rem 3rem
+  .item-chlidren
+    padding 0 .2rem
+</style>
